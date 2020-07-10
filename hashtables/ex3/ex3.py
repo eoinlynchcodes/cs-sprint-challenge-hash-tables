e4c26714bf38eceb91b352adcf88bd70293f9c31
eoinlynchcodes <eoin95lynch@gmail.com>
2020-07-10 11:01:22 +0100

    Go

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,15 +2,27 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
+    # cache = []
+    # for array in arrays:
+    #     for value in array:
+    #         if value not in cache:
+    #             cache.append(value)
+    #         else:
+    #             return cache
 
     cache = []
 
-    for array in arrays:
-        for value in array:
-            if value not in cache:
-                cache.append(value)
-            else:
-                return cache
+    for i, array in enumerate(arrays):
+        print(i, array)
+        for i, elem in enumerate(array):
+            print(i, elem)
+            if elem not in cache:
+                cache[elem] = []
+
+            cache[elem].append[i]
+
+    return cache
+        
 
 print(intersection([
         [1,2,4],
